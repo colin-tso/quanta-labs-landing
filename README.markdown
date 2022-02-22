344b5f2a2302fa7a092eb250de04f178cdfbc3d1
GrandTheftGelato <colintso@hotmail.com>
2022-02-22 21:17:34 +1100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,1 @@
+Demo landing page for Quanta Labs
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var postcss_nested = require('postcss-nested');
+var sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp');
+var processors = [
+    autoprefixer,
+    postcss_nested
+];
+
+const rename = require('gulp-rename');
+
+
+
+gulp.task('pcss_to_css', function () {
+    return gulp.src('./src/*.pcss')
+        .pipe(sourcemaps.init())
+        .pipe(postcss(processors))
+        .pipe(rename({
+            extname: '.css'
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dest'));
+});
